9da6146bc9144b7b42cf9935265ed690796c2a84
LeozinGs <lokinho_ddtank@hotmail.com>
2024-11-28 17:43:29 -0300

    Cria componente estilizado do Header e deleta styles.css referente ao mesmo.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,23 @@
-import './styles.css';
 import Logo from '../Logo/Logo';
 import ListOptions from '../ListOptions/ListOptions';
 import ListIcons from '../ListIcons/ListIcons';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+    padding: 1em .5em;
+    background-color: #fff;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+`;
 
 const Header = () => {
     return (
-        <header className='app-header'>
+        <HeaderContainer>
             <Logo />
             <ListOptions />
             <ListIcons />
-        </header>
+        </HeaderContainer>
     );
 }
 
