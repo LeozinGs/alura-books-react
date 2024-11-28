f43accc79bbb04a9dd3ad86b490d1b201ece1443
LeozinGs <lokinho_ddtank@hotmail.com>
2024-11-28 18:14:34 -0300

    Cria estilos globais utilizando o Styled Components e deleta arquivo index.css do projeto.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.jsx';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+      --font-family: 'Arial';
+  }
+
+  * {
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+  }
+
+  body {
+      font-family: var(--font-family);
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+  }
+
+  li {
+      list-style: none;
+  }
+`;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GlobalStyle />
     <App />
   </StrictMode>,
 );
