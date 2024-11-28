e52e9199c60ce056ba9a04e548a5023385cc3923
LeozinGs <lokinho_ddtank@hotmail.com>
2024-11-28 17:52:12 -0300

    Cria componente estilizado de ListIcons e deleta arquivo .css do mesmo.

--- src/components/ListIcons/ListIcons.jsx
+++ src/components/ListIcons/ListIcons.jsx
@@ -1,6 +1,19 @@
-import './styles.css';
 import profile from '../../assets/perfil.svg';
 import shopCart from '../../assets/sacola.svg';
+import styled from 'styled-components';
+
+const Icons = styled.ul`
+    display: flex;
+    gap: 2em;
+    justify-content: center;
+    align-items: center;
+    padding: 0 1em;
+`;
+
+const Icon = styled.li`
+    cursor: pointer;
+    user-select: none;
+`;
 
 const icons = [
     shopCart,
@@ -9,14 +22,12 @@ const icons = [
 
 const ListIcons = () => {
     return (
-        <ul className='header-icons'>
+        <Icons className='header-icons'>
             {icons.map((item, index) => {
-                return <li key={index} className='icons-item'>
-                    <img src={item} />
-                </li>
+                return <Icon key={index}><img src={item} /></Icon>
             })
             }
-        </ul>
+        </Icons>
     );
 }
 
